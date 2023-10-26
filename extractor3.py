8cec1fa21fdd9ea34b0c692e7228df101419f4b2
Jonathan Will <jonathan.will@fau.de>
2023-10-26 22:52:59 +0200

    following fixes & updates: added and fixed scrollbars, the selected image is no more resized for the canvas, the image keeps its original size

--- extractor3.py
+++ extractor3.py
@@ -24,9 +24,9 @@ def load_image(filepath=None):
         print(f"loading file {filepath}")
         image_original=Image.open(filepath) # load file when path is specified
         print(f"resizing file {filepath}")
-        img_resized = image_original.resize((500,500), Image.Resampling.LANCZOS) # create resized copy of image 
+        #img_resized = image_original.resize((500,500), Image.Resampling.LANCZOS) # create resized copy of image 
 
-    image_tk = ImageTk.PhotoImage(img_resized) # create tk image object
+    image_tk = ImageTk.PhotoImage(image_original) # create tk image object
 
     if center_canvas_container == None:
         print(f'no image previously loaded, so a blank canvas container is created')
@@ -35,6 +35,11 @@ def load_image(filepath=None):
     else:
         print(f'rewriting already existing container')
         gui_center_canvas.itemconfig(center_canvas_container, image=image_tk)
+
+    # reconfigure gui elements:
+    gui_center_image_xscroll.config(command=gui_center_canvas.xview)
+    gui_center_image_yscroll.config(command=gui_center_canvas.yview)
+    gui_center_canvas.config(scrollregion=gui_center_canvas.bbox(tk.ALL), width=int(gui_window.winfo_width()*0.25), height=int(gui_window.winfo_height()*0.8))
 
 
 # METHODS: BUTTONS
@@ -110,17 +115,28 @@ gui_controlls_button_clear.grid(column=2, row=2, sticky=tk.W+tk.E)
 # FRAME: CENTER
 # center frame: widgets
 gui_label_2 = tk.Label(gui_frame_center, text="Working Image", bg="orange", fg="white")
-gui_center_canvas = tk.Canvas(gui_frame_center, height=500, width=500)
+gui_center_image_xscroll = tk.Scrollbar(gui_frame_center, orient=tk.HORIZONTAL) # scroll bar for canvas in x direction
+gui_center_image_yscroll = tk.Scrollbar(gui_frame_center) # scroll bar for canvas in y direction
+gui_center_canvas = tk.Canvas(gui_frame_center, width=100, height=100, xscrollcommand=gui_center_image_xscroll.set, yscrollcommand=gui_center_image_yscroll.set) # central image cnavas
+
+
+# center frame: config
+gui_center_canvas.config(scrollregion=gui_center_canvas.bbox(tk.ALL))
+gui_center_image_xscroll.config(command=gui_center_canvas.xview)
+gui_center_image_yscroll.config(command=gui_center_canvas.yview)
 
 
 # center frame: widgets layout
-gui_label_2.grid(column=0, row=0,sticky=tk.N+tk.S+tk.W)
-gui_center_canvas.grid(column=0, row=1, sticky=tk.N+tk.S+tk.W)
+gui_frame_center.columnconfigure(0, weight=3)
+gui_label_2.grid(row=0, column=0, sticky=tk.N+tk.S+tk.W)
+gui_center_canvas.grid(row=1, column=0, sticky=tk.N+tk.S+tk.W+tk.E)
+gui_center_image_xscroll.grid(row=2, column=0, sticky=tk.E+tk.W)
+gui_center_image_yscroll.grid(row=1, column=1, sticky=tk.N+tk.S)
 
 
 # FRAME EVALUATION
 # evaluation frame: widgest
-gui_label_3 = tk.Label(gui_frame_evaluation, text="Results", bg="green", fg="white")
+gui_label_3 = tk.Label(gui_frame_evaluation, text="Results, Evaluation & Metadata", bg="green", fg="white")
 
 # evaluation frame: widgets layout
 gui_label_3.grid(column=0, row=0,sticky=tk.N+tk.S)
